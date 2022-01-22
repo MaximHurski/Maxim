26fefc9f9eeb6e4faef4182f02aac29428c20521
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-22 22:21:46 +0300

    Update Task_4_4.java

--- src/com/company/Lessons/les4/Homework/Task_4_4.java
+++ src/com/company/Lessons/les4/Homework/Task_4_4.java
@@ -3,11 +3,13 @@ package com.company.Lessons.les4.Homework;
 //Начав тренировки, спортсмен в первый день пробежал 10 км.
 //Каждый день он увеличивал дневную норму на 10% нормы предыдущего дня.
 //Какой суммарный путь пробежит спортсмен за 7 дней?
-
+//+
 public class Task_4_4 {
     public static void main(String[] args) {
+        //use better naming and there will be no need in commets
         double sum = 10;     //Итоговый пробег (в первый день 10км)
         double n = 10;       // % увеличения нормы
+        //7 - magic number
         for (double i = 1; i < 7; i++) {
             n = n * 1.1;
             sum += n;
