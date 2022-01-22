49aa9b7f308b8a654efbbe02cd1736f93154768c
max <maxhurski@gmail.com>
2022-01-22 12:20:15 +0300

    first task

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+}}
+
+
