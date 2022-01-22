71c00360960a01e5ea10d56f3ba3707e5dc0e3e9
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-22 22:20:05 +0300

    Update Task_4_2.java

--- src/com/company/Lessons/les4/Homework/Task_4_2.java
+++ src/com/company/Lessons/les4/Homework/Task_4_2.java
@@ -8,12 +8,15 @@ public class Task_4_2 {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter first number:");
+        //naming could be better
         int num1 = scan.nextInt();
         System.out.println("Enter second number:");
         int num2 = scan.nextInt();
 
         int sum = 0;
+        //or i<num1
         for (int i = 0; i != num1; i++ ) {
+            //sum+=num2
             sum = sum + num2;
         }
         System.out.println("Multiplication number1 and number2 is = " + sum);
