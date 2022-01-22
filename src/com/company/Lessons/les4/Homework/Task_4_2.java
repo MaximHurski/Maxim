4d2492392f1775841e59c3764fbdd5b40fc307a1
max <maxhurski@gmail.com>
2022-01-22 17:56:06 +0300

    lesson4

---
+++ src/com/company/Lessons/les4/Homework/Task_4_2.java
@@ -0,0 +1,22 @@
+package com.company.Lessons.les4.Homework;
+
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Task_4_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        int num1 = scan.nextInt();
+        System.out.println("Enter second number:");
+        int num2 = scan.nextInt();
+
+        int sum = 0;
+        for (int i = 0; i != num1; i++ ) {
+            sum = sum + num2;
+        }
+        System.out.println(sum);
+        }
+    }
+
