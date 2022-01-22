d04591f04333ea2bf48120680a18fc4b0fd6c0a2
max <maxhurski@gmail.com>
2022-01-22 17:16:38 +0300

    first task

---
+++ src/com/company/Lessons/les4/Homework/Task_4_3.java
@@ -0,0 +1,11 @@
+package com.company.Lessons.les4.Homework;
+
+public class Task_4_3 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i<=20; i++) {
+            System.out.println (i + " inch = " + 2.54*i + " cm");
+        }
+    }
+}
+
