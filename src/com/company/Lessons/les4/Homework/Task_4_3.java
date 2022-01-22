d5142266708f61326f1d5ff378d3993f9233daff
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-22 22:20:39 +0300

    Update Task_4_3.java

--- src/com/company/Lessons/les4/Homework/Task_4_3.java
+++ src/com/company/Lessons/les4/Homework/Task_4_3.java
@@ -5,7 +5,7 @@ package com.company.Lessons.les4.Homework;
 
 public class Task_4_3 {
     public static void main(String[] args) {
-
+//20, 2.54 are magic numbers, use const
         for (int i = 1; i <= 20; i++) {
             System.out.println (i + " inch = " + 2.54*i + " cm");
         }
