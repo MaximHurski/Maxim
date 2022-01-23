c51724ed19bd849739e01bee94f1867f103b317d
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-23 22:00:53 +0300

    Update Task_5_3.java

--- src/com/company/Lessons/les5/Homework/Task_5_3.java
+++ src/com/company/Lessons/les5/Homework/Task_5_3.java
@@ -8,8 +8,10 @@ public class Task_5_3 {
         int i = 0;
         int sum = 0;
         final int max = 99;
+        //I agree that this solution works, but think of a better one
         while (i <= max) {
             i++;
+            //why do you need to put it in block of code?
             {
                 if (i % 2 == 0)
                     continue;
