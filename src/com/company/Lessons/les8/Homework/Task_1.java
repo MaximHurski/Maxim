4de064528878f3d71017be68e9a34b58a11039cb
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-02-05 18:52:45 +0300

    Update Task_1.java

--- src/com/company/Lessons/les8/Homework/Task_1.java
+++ src/com/company/Lessons/les8/Homework/Task_1.java
@@ -4,6 +4,7 @@ package com.company.Lessons.les8.Homework;
 public class Task_1 {
     public static void main(String[] args) {
         String[] seasons = new String[]{"Winter", "Spring", "Summer", "Autumn"};
+        //as one of the possible solutions, ok
         for (int i = 3; i >= 0; i--) {
             System.out.println(seasons[i]);
         }
