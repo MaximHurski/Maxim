5d31be8f60e1c4a7bfd86282131ae13886f527de
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-02-05 18:58:01 +0300

    Update Task_2.java

--- src/com/company/Lessons/les8/Homework/Task_2.java
+++ src/com/company/Lessons/les8/Homework/Task_2.java
@@ -6,14 +6,17 @@ import java.util.Arrays;
 public class Task_2 {
     public static void main(String[] args) {
         int[] anArray1 = {13, 44, 2, 45, 87, 23, 222, 33, 24, 35, 5, 6, 7, 8, 88, 111, 102};
+        //BIG LETTER? Don't scare me:)
         int MaxEven = 0;
         for (int i = 0; i < anArray1.length; i++) {
             if (anArray1[i] % 2 == 0) {
                 MaxEven++;
             }
         }
+        //nice
         System.out.println(Arrays.toString(anArray1));
         int[] anArray2 = new int[MaxEven];
+        //BIG LETTER???
         int Even = 0;
         for (int i = 0; i < anArray1.length; i++) {
             if (anArray1[i] % 2 == 0) {
