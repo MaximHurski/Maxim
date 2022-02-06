bae433ef9ed6ffcc6645b82583ed2fe97582099b
max <maxhurski@gmail.com>
2022-02-06 10:32:17 +0300

    new

--- src/com/company/Lessons/les8/Homework/Task_2.java
+++ src/com/company/Lessons/les8/Homework/Task_2.java
@@ -7,21 +7,21 @@ public class Task_2 {
     public static void main(String[] args) {
         int[] anArray1 = {13, 44, 2, 45, 87, 23, 222, 33, 24, 35, 5, 6, 7, 8, 88, 111, 102};
         //BIG LETTER? Don't scare me:)
-        int MaxEven = 0;
+        int maxEven = 0;
         for (int i = 0; i < anArray1.length; i++) {
             if (anArray1[i] % 2 == 0) {
-                MaxEven++;
+                maxEven++;
             }
         }
         //nice
         System.out.println(Arrays.toString(anArray1));
-        int[] anArray2 = new int[MaxEven];
+        int[] anArray2 = new int[maxEven];
         //BIG LETTER???
-        int Even = 0;
+        int even = 0;
         for (int i = 0; i < anArray1.length; i++) {
             if (anArray1[i] % 2 == 0) {
-                anArray2[Even] = anArray1[i];
-                Even++;
+                anArray2[even] = anArray1[i];
+                even++;
             }
         }
         System.out.println(Arrays.toString(anArray2));
